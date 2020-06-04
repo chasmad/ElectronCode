1984931f8a6c22d3fb88414be5e598506c2d11d8
Daniel Beck <dlc.beck@gmail.com>
2020-06-04 10:58:06 -0700

    initial commit

---
+++ boilerplates/tasky/index.js
@@ -0,0 +1,14 @@
+const electron = require('eletron');
+
+const { app, BrowserWindow } = electron;
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
