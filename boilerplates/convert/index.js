23cb41b2ace79d105f67c5ad66a26591fb93c801
Daniel Beck <dlc.beck@gmail.com>
2020-06-05 14:28:57 -0700

    add open in folder functionality

--- boilerplates/convert/index.js
+++ boilerplates/convert/index.js
@@ -1,7 +1,7 @@
 const electron = require('electron');
 const ffmpeg = require('fluent-ffmpeg');
 const _ = require('lodash');
-const { app, BrowserWindow, ipcMain } = electron;
+const { app, BrowserWindow, ipcMain, shell } = electron;
 
 let mainWindow;
 
@@ -53,4 +53,8 @@ ipcMain.on('conversion:start', (event, videos) => {
             )
             .run();
     });
+});
+
+ipcMain.on('folder:open', (event, outputPath) => {
+    shell.showItemInFolder(outputPath);
 });
